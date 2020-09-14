1fd605629f81cc46890d3f970694df456281dd52
邹磊 <spx-zoul@supaur.com>
2020-09-14 23:17:19 +0800

    二叉树

--- leetCode/src/Solution.java
+++ leetCode/src/Solution.java
@@ -1,14 +1,25 @@
-import leetCode.OneHundredAndNinetyEight;
-import leetCode.ThirtyThree;
+import leetCode.*;
 
 public class Solution {
 
     public static void main(String[] args) {
 
 
-        ThirtyThree s = new ThirtyThree();
-        int res = s.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4});
-        System.out.println(res);
+        SixHundredAndSeventeen s = new SixHundredAndSeventeen();
+
+        TreeNode t1 = new TreeNode(1);
+        t1.setLeft(new TreeNode(3));
+        t1.getLeft().setLeft(new TreeNode(5));
+        t1.setRight(new TreeNode(2));
+
+        TreeNode t2 = new TreeNode(2);
+        t2.setLeft(new TreeNode(1));
+        t2.getLeft().setRight(new TreeNode(4));
+        t2.setRight(new TreeNode(3));
+        t2.getRight().setRight(new TreeNode(7));
+
+        s.mergeTrees(t1,t2);
+        System.out.println("");
 
     }
 
---
+++ leetCode/src/leetCode/SixHundredAndSeventeen.java
@@ -0,0 +1,44 @@
+package leetCode;
+
+/**
+ * https://leetcode-cn.com/problems/merge-two-binary-trees/submissions/
+ * 为什么下面那种方法不行？
+ */
+public class SixHundredAndSeventeen {
+
+
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+
+        if (t1 == null)
+            return t2;
+        if (t2 == null)
+            return t1;
+
+        t1.val = t1.val + t2.val;
+        t1.left = mergeTrees(t1.left,t2.left);
+        t1.right = mergeTrees(t1.right,t2.right);
+
+        return t1;
+    }
+
+    public void merge(TreeNode t1, TreeNode t2){
+        if (t1 == null && t2 == null) {
+            t1 = null;
+            return;
+        }
+        if (t1 == null){
+            t1 = t2;
+            return;
+        }
+        if (t2 == null){
+            t1 = t1;
+            return;
+        }
+
+        t1.val = t1.val + t2.val;
+        merge(t1.left,t2.left);
+        merge(t1.right,t2.right);
+    }
+
+}
