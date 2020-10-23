6ef2229edbb64938ec5e2dcd3ff94a8b7b736237
邹磊 <spx-zoul@supaur.com>
2020-10-23 09:09:44 +0800

    数组数组

---
+++ leetCode/src/leetCode/FourHundredAndFortyTwo.java
@@ -0,0 +1,50 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 邹磊
+ * @version 1.0
+ * @description:
+ * @date 2020/10/22 19:38
+ */
+public class FourHundredAndFortyTwo {
+
+    public List<Integer> findDuplicates(int[] nums) {
+
+        List<Integer> result = new ArrayList<>();
+
+
+
+        for (int i = 0; i < nums.length; i++ ){
+
+            if (nums[Math.abs(nums[i])-1] < 0 ){
+                result.add(Math.abs(nums[i]));
+            } else {
+                nums[Math.abs(nums[i])-1] *= -1;
+            }
+
+        }
+
+
+//        for (int num : nums) {
+//
+//            while (num > 0){
+//                if (nums[num-1] == -num){
+//                    result.add(num);
+//                    num = 0;
+//                } else {
+//                    int tmp = nums[num-1];
+//                    nums[num-1] = -num;
+//                    num = tmp;
+//                }
+//            }
+//        }
+
+        return result;
+    }
+}
+
+
+
---
+++ leetCode/test/leetCode/FourHundredAndFortyTwoTest.java
@@ -0,0 +1,17 @@
+package leetCode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FourHundredAndFortyTwoTest {
+
+    @Test
+    public void findDuplicates() {
+
+        FourHundredAndFortyTwo s = new FourHundredAndFortyTwo();
+
+        s.findDuplicates(new int[]{4,3,2,7,8,2,3,1});
+
+    }
+}
