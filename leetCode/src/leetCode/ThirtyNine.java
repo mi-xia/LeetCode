cbfae2359fc407fc89399ffed1d8b9430bf95e54
邹磊 <spx-zoul@supaur.com>
2020-11-05 20:28:11 +0800

    回溯法

---
+++ leetCode/src/leetCode/ThirtyNine.java
@@ -0,0 +1,85 @@
+package leetCode;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author 邹磊
+ * @version 1.0
+ * @description: https://leetcode-cn.com/problems/combination-sum/
+ * 难点：如何去重结果集？
+ * @date 2020/11/5 19:27
+ */
+public class ThirtyNine {
+
+    private List<List<Integer>> result = new ArrayList<>();
+
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        doCombinationSum(candidates,target,new ArrayList<>(),0);
+
+        return result;
+
+    }
+
+
+    void doCombinationSum(int[] candidates, int target, List<Integer> tmp, int index){
+
+        if (target < 0){
+            return;
+        }
+
+        if (target == 0){
+            List<Integer> res = new ArrayList<>();
+            res.addAll(tmp);
+            result.add(res);
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++){
+            tmp.add(candidates[i]);
+            doCombinationSum(candidates,target - candidates[i],tmp,i);
+            tmp.remove(tmp.size()-1 < 0 ? 0 : tmp.size()-1);
+        }
+
+    }
+
+}
+
+
+//
+//    private List<List<Integer>> result = new ArrayList<>();
+//
+//
+//    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//
+//        doCombinationSum(candidates,target,new ArrayList<>(),0);
+//
+//        return result;
+//
+//    }
+//
+//
+//    void doCombinationSum(int[] candidates, int target, List<Integer> tmp, int index){
+//
+//        if (target < 0){
+//            return;
+//        }
+//
+//        if (target == 0){
+//            List<Integer> res = new ArrayList<>();
+//            res.addAll(tmp);
+//            result.add(res);
+//            return;
+//        }
+//
+//        for (int i = index; i < candidates.length; i++){
+//            tmp.add(candidates[i]);
+//            doCombinationSum(candidates,target - candidates[i],tmp,i);
+//            tmp.remove(tmp.size()-1 < 0 ? 0 : tmp.size()-1);
+//        }
+//
+//    }
---
+++ leetCode/test/leetCode/ThirtyNineTest.java
@@ -0,0 +1,16 @@
+package leetCode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ThirtyNineTest {
+
+    @Test
+    public void combinationSum() {
+
+        ThirtyNine thirtyNine = new ThirtyNine();
+        thirtyNine.combinationSum(new int[]{2,3,6,7},7);
+
+    }
+}
