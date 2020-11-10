36c775904df319a5d6de2ffe4afcbf9624e18ac7
邹磊 <spx-zoul@supaur.com>
2020-11-10 19:42:26 +0800

    好好利用map

---
+++ leetCode/src/leetCode/FortyNine.java
@@ -0,0 +1,37 @@
+package leetCode;
+
+import com.google.common.primitives.Chars;
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
+import java.util.*;
+
+/**
+ * @author 邹磊
+ * @version 1.0
+ * @description: https://leetcode-cn.com/problems/group-anagrams/comments/
+ * @date 2020/11/10 19:11
+ */
+public class FortyNine {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String,List<String>> result = new HashMap<>();
+
+        for (String s : strs){
+            char[] tmp = s.toCharArray();
+            Arrays.sort(tmp);
+            String key = new String(tmp);
+            if (null != result.get(key)){
+                List<String> vaule = new ArrayList<>(result.get(key));
+                vaule.add(s);
+                result.put(key,vaule);
+            } else {
+                result.put(key, Arrays.asList(s));
+            }
+        }
+
+        return new ArrayList<>(result.values());
+
+    }
+
+}
---
+++ leetCode/test/leetCode/FortyNineTest.java
@@ -0,0 +1,16 @@
+package leetCode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FortyNineTest {
+
+    @Test
+    public void groupAnagrams() {
+        FortyNine fortyNine = new FortyNine();
+
+        fortyNine.groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"});
+
+    }
+}
